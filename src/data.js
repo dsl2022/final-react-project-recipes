a2e29cfd67d1d575cf6ef2ada33d82f8eb225468
dsl2055 <job2019tmm@gmail.com>
2022-08-16 18:45:28 -0400

    updated

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default data = [
+  {
+    title: "Classic Kimbap (Korean Seaweed Rice Roll)",
+    description:
+      "You'll want to make a double batch of this tortellini salad—it tastes even better the next day and it's the perfect packed lunch for school or work.",
+    photoUrl:
+      "https://www.simplyrecipes.com/thmb/ncWGB_phVPoFnA1rtUQWuqugSug=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Kimbap-LEAD-09-773af5d7108b4410acdbf3935a27e225.jpg",
+  },
+  {
+    title: "A Love Letter to Wonder Girl, on the Eve of First Grade",
+    description:
+      "You'll want to make a double batch of this tortellini salad—it tastes even better the next day and it's the perfect packed lunch for school or work.",
+    photoUrl:
+      "https://www.simplyrecipes.com/thmb/VzUZ6KPVC6ACF797V35Ib2-pKEM=/736x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-LLL-Dear-Daughter-ANIM-4f8091891ab14cd5aff3c8374d148a1b.gif",
+  },
+  {
+    title: "Chickpea Panini",
+    description:
+      "You'll want to make a double batch of this tortellini salad—it tastes even better the next day and it's the perfect packed lunch for school or work.",
+    photoUrl:
+      "https://www.simplyrecipes.com/thmb/WeD15WQwua2uBsOarBLN6zDqCRc=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Chickpea-Panini-LEAD-5-6c6967590c8d43a9ac46a49cbae06671.jpg",
+  },
+  {
+    title: "Loaded Veggie Sandwich",
+    description:
+      "You'll want to make a double batch of this tortellini salad—it tastes even better the next day and it's the perfect packed lunch for school or work.",
+    photoUrl:
+      "https://www.simplyrecipes.com/thmb/onJ-wpqMYUF3x5HTvs08imU0cis=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Loaded-Veggie-Sandwich-LEAD-6-cbf54f10c0294daaa647e11321c73773.jpg",
+  },
+  {
+    title: "Chickpea Panini",
+    description:
+      "You'll want to make a double batch of this tortellini salad—it tastes even better the next day and it's the perfect packed lunch for school or work.",
+    photoUrl:
+      "https://www.simplyrecipes.com/thmb/WeD15WQwua2uBsOarBLN6zDqCRc=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Chickpea-Panini-LEAD-5-6c6967590c8d43a9ac46a49cbae06671.jpg",
+  },
+  {
+    title: "Tortellini Salad",
+    description:
+      "You'll want to make a double batch of this tortellini salad—it tastes even better the next day and it's the perfect packed lunch for school or work.",
+    photoUrl:
+      "https://www.simplyrecipes.com/thmb/pFIQA_-jljDGyK2RTaLd5hfknY0=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Tortellini-LEAD-1-8a494353aca4448eac6fce60a94edeaa.jpg",
+  },
+];
